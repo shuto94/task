4b1b574d413c3ba8cc2ceac83e2508fa84bfcc68
shuto94 <kbmc1403@gmail.com>
2021-04-18 16:30:15 +0900

    バリデーション

--- config.php
+++ config.php
@@ -4,5 +4,8 @@ define('DSN', 'mysql:host=db;dbname=task_app;charset=utf8');
 define('USER', 'testuser');
 define('PASSWORD', '9999');
 
+//エラーメッセージを定数として定義
+define('MSG_TITLE_REQUIRED', 'タスク名を入力してください');
+
 //ステータスを定数として定義
 define('TASK_STATUS_NOTYET' , 'notyet');
